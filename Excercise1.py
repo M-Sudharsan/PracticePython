4c3c742cf1c43a03aa6534bf53f7ba300a8f5f11
SUDHARSAN MURALI <40484760+M-Sudharsan@users.noreply.github.com>
2020-09-09 11:43:19 +0530

    Update Excercise1.py

--- Excercise1.py
+++ Excercise1.py
@@ -2,6 +2,8 @@
 #Extras:
 #Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
 #Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
+
+
 name = input("Give me your name: ")
 age = int(input("Give me your age: "))
 remaining_years = 100 - age
