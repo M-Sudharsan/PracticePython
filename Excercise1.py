e6fb25e6405f8109e2100e8edbe90ce215de4863
SUDHARSAN MURALI <40484760+M-Sudharsan@users.noreply.github.com>
2020-09-09 11:51:09 +0530

    Update Excercise1.py

--- Excercise1.py
+++ Excercise1.py
@@ -6,6 +6,6 @@
 name = input("Give me your name: ")
 age = int(input("Give me your age: "))
 remaining_years = 100 - age
-Year100 = 2020 + remaining_years
+Year100 = 2020 + remaining_years              #The year in which age 100 is reached
 Number = int(input("Give number of times to print msg: "))
 print(("Your name is "+name+" and you will turn 100 in the year:"+str(Year100)+"\n")*Number)
